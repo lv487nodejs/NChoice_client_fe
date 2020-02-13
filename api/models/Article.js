69649b95a6fb669ddd6173a23655a2758e992e75
Markiyan Shestakovych <markiyan89@gmail.com>
2020-02-13 13:40:48 +0200

    tabWidth change

--- api/models/Article.js
+++ api/models/Article.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ArticleSchema = new Schema({
-        name: {
-                type: String,
-                required: true,
-                unique: true,
-        },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 });
 
 const Article = mongoose.model('order', ArticleSchema);
