fd9435e210f7dbe5f980b48f15d2a84d6ab8e4d4
Markiyan Shestakovych <markiyan89@gmail.com>
2020-02-13 22:35:11 +0200

    eslinted

--- server.js
+++ server.js
@@ -14,15 +14,12 @@ const app = express();
 connectDB();
 
 app.use(morgan('dev'));
-app.use(morgan('common'))
-app.use(morgan('short'))
+app.use(morgan('common'));
+app.use(morgan('short'));
 
-// here custom token is created. instead of "body" you can use any object from "req" 
-morgan.token('body',(req,res)=>{
-    return console.log(JSON.stringify(req.body));
-    
-})
-app.use(morgan(' :method :url  :body :response-time '))
+// here custom token is created. instead of "body" you can use any object from "req"
+morgan.token('body', (req, res) => console.log(JSON.stringify(req.body)));
+app.use(morgan(' :method :url  :body :response-time '));
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
