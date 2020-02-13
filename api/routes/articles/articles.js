5e29315c686af411e7b2a520f932b8c92e3c5cc5
oleg <reekee94@gmail.com>
2020-02-13 14:45:12 +0200

    users routes

--- api/routes/articles/articles.js
+++ api/routes/articles/articles.js
@@ -3,4 +3,32 @@ const ArticleModel = require('../../models/Article');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        return res.send('Products get OK');
+    } catch (err) {
+        return console.log(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    try {
+        let product = await ProductModel.findOne({ name });
+        if (product) {
+            return res.status(400).json({ errors: [{ msg: 'Cloth item already exists' }] });
+        }
+
+        product = new ProductModel(req.body);
+
+        await product.save();
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+
+
 module.exports = router;
