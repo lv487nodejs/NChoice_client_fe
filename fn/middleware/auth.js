1f3e13750556d87a0d3ce548262c06095d08bd42
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2020-02-17 14:17:59 +0200

    hotfix

--- fn/middleware/auth.js
+++ fn/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
 
     if (!token) {
@@ -9,7 +9,7 @@ module.exports = function(req, res, next) {
     }
 
     try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         req.user = decoded.user;
         next();
