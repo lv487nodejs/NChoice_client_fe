d48f334f97ec0442cbc1080d7d755ca853ed4cc2
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2020-02-20 17:12:05 +0200

    remove unused import

--- fn/routes/products/brands.js
+++ fn/routes/products/brands.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { brandValidationRules, brandQueryRules, validate } = require('../../middleware/validator');
+const { brandValidationRules, validate } = require('../../middleware/validator');
 
 const Brands = require('../../models/Brand');
 
