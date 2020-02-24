ca7fe4b8e18a063ae181c9fcb964bb68a92e3450
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2020-02-24 23:26:48 +0200

    add catalog service

--- fn/client/src/components/category-items/Сategory-items.js
+++ fn/client/src/components/category-items/Сategory-items.js
@@ -7,16 +7,11 @@ import CategoryCarousel from '../category-carousel';
 export const CategoryItems = props => {
     const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://stark-headland-06017.herokuapp.com/catalogs?catalog=${props.catalogName}`, {
-            method: 'GET',
-        })
-            .then(res => res.json())
-            .then(response => {
-                setProducts(response[0].categories);
-            })
-            .catch(error => console.log(error));
-    }, [props, props.catalogName]);
+    useEffect(async () => {
+        console.log(CatalogService);
+        const catalogs = await сatalogService.getCatalogByName(props.catalogName);
+        setProducts(catalogs[0].categories)
+    }, [props.catalogName]);
 
     return (
         <div className="categories">
