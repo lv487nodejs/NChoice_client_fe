302b1e4f5fa33ed115b785689b4900d71d516d0b
chak-kit <kitchakk93@gmail.com>
2020-02-25 17:06:32 +0200

    add redux folders

--- fn/client/src/index.js
+++ fn/client/src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
+import store from './store/store';
+import Provider from 'react-redux/es/components/Provider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+
+
