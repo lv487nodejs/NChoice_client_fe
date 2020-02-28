0ae85b22f2c495daef51b6473654e3b439cc4c4f
bandvov <48312647+bandvov@users.noreply.github.com>
2020-02-28 14:11:31 +0200

    upd

--- fn/client/src/actions/index.js
+++ fn/client/src/actions/index.js
@@ -3,6 +3,20 @@ import { productsLoaded, productsRequested } from './Products-actions';
 
 import { catalogLoaded, catalogsLoaded, catalogsRequested } from './Catalogs-actions';
 
+import {
+    filterAddBrand,
+    filterAddCategory,
+    filterAddColor,
+    filterRemoveCategory,
+    filterRemoveColor,
+    filterRemoveBrand,
+    composeFilters,
+    fetchSuccessBrands,
+    fetchSuccessCategories,
+    fetchSuccessColors,
+    composeReceivedData,
+} from './Filter-actions';
+
 export {
     categoriesLoaded,
     categoriesRequested,
@@ -11,4 +25,15 @@ export {
     catalogLoaded,
     catalogsLoaded,
     catalogsRequested,
+      filterAddBrand,
+    filterAddCategory,
+    filterAddColor,
+    filterRemoveBrand,
+    filterRemoveCategory,
+    filterRemoveColor,
+    composeFilters,
+    fetchSuccessBrands,
+    fetchSuccessCategories,
+    fetchSuccessColors,
+    composeReceivedData,
 };
