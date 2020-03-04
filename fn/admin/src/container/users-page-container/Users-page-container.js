a5838775f845a5bbaf6355cf5d8945bd362889ae
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2020-03-04 13:42:45 +0200

    add users container

---
+++ fn/admin/src/container/users-page-container/Users-page-container.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const UsersPageContainer = () => <Typography>UsersPageContainer</Typography>;
+
+export default UsersPageContainer;
