89cbf489bd04519333726152fe983191ea0e4b15
Markiyan Shestakovych <markiyan89@gmail.com>
2020-03-06 14:03:35 +0200

    Table Nav

---
+++ fn/admin/src/components/table-nav/Table-nav.js
@@ -0,0 +1,154 @@
+import React from 'react';
+
+import {
+    Button,
+    ButtonGroup,
+    Menu,
+    MenuItem,
+    FormControlLabel,
+    Checkbox,
+    InputBase,
+    Grid,
+    FormGroup,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyle from './Table-nav-style';
+
+const TableNav = () => {
+    const classes = useStyle();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true,
+        checkedF: true,
+        checkedG: true,
+    });
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleChange = name => event => {
+        setState({ ...state, [name]: event.target.checked });
+    };
+    return (
+        <Grid container spacing={3} justify="center" alignItems="center">
+            <Grid item xs={2}>
+                <Button variant="contained" color="primary" size="small">
+                    NEW PRODUCT
+                </Button>
+            </Grid>
+            <Grid item xs={4}>
+                <ButtonGroup
+                    variant="contained"
+                    color="primary"
+                    aria-label="contained primary button group"
+                >
+                    <Button
+                        size="small"
+                        aria-controls="simple-menu"
+                        onClick={handleClick}
+                    >
+                        Catalogs
+                    </Button>
+                    <Menu
+                        elevation={0}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
+                        }}
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <FormGroup column>
+                            <MenuItem>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={state.checkedA}
+                                            onChange={handleChange('checkedA')}
+                                            value="checkedA"
+                                            size="small"
+                                        />
+                                    }
+                                    label="Secondary"
+                                    size="small"
+                                />
+                            </MenuItem>
+                            <MenuItem>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={state.checkedB}
+                                            onChange={handleChange('checkedB')}
+                                            value="checkedB"
+                                            size="small"
+                                        />
+                                    }
+                                    label="Secondary"
+                                />
+                            </MenuItem>
+                            <MenuItem>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={state.checkedC}
+                                            onChange={handleChange('checkedC')}
+                                            value="checkedC"
+                                            size="small"
+                                        />
+                                    }
+                                    label="Secondary"
+                                />
+                            </MenuItem>
+                            <MenuItem>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={state.checkedD}
+                                            onChange={handleChange('checkedD')}
+                                            value="checkedD"
+                                            size="small"
+                                        />
+                                    }
+                                    label="Secondary"
+                                />
+                            </MenuItem>
+                        </FormGroup>
+                    </Menu>
+                    <Button size="small">Categories</Button>
+                    <Button size="small">Brands</Button>
+                </ButtonGroup>
+            </Grid>
+            <Grid item xs={3}>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon />
+                    </div>
+                    <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </div>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default TableNav;
--- fn/admin/src/container/products-page-container/Products-page-container.js
+++ fn/admin/src/container/products-page-container/Products-page-container.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useStyles } from './Products-page-container-styles';
 import ProductList from '../../components/product-list/Product-list';
+import TableNav from '../../components/table-nav';
 
 const ProductsPageContainer = () => {
     const classes = useStyles();
 
     return (
         <div className={classes.content}>
+            <TableNav />
             <ProductList />
         </div>
     );
