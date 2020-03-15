e1919381d8c8ac77bc43d20102e883e3c94687ed
chak-kit <kitchakk93@gmail.com>
2020-03-15 21:38:17 +0200

    refactoring

--- fn/client/src/components/cart/Cart.js
+++ fn/client/src/components/cart/Cart.js
@@ -6,32 +6,21 @@ import Row from "react-bootstrap/Row";
 import Container from "@material-ui/core/Container/Container";
 
 const Cart = ({cartProps}) => {
-  let cart = new Map();
-  for (let product of cartProps.products) {
-    let foundProduct = cart.get(product.id);
-    if (foundProduct) {
-      foundProduct.quantity++;
-    } else {
-      cart.set(product.id, {product, "quantity": 1})
-    }
-  }
-
-  console.log("=======");
-  console.log(cart);
+  console.log(cartProps.products);
 
   return (
     <div className='main-cart'>
       <h3>Cart</h3>
-      <h5>{cart.size < 1 && <em> Please add some products to cart.</em> }</h5>
+      <h5>{cartProps.products.length < 1 && <em> Please add some products to cart.</em> }</h5>
       <ul className='cart-wrap'>
-        {Array.from(cart).map(([key, value]) => (
-          <li key={key} className='cart-item'>
+        {cartProps.products.map((value) => (
+          <li key={value.id} className='cart-item'>
             <Container>
               <Row>
-                <Figure.Image src={`/images/products/${value.product.images[0]}`} className='cartImg'/>
+                <Figure.Image src={`/images/products/${value.images[0]}`} className='cartImg'/>
                 <Figure.Caption className='cartTitle'>
-                  {value.product.title}
-                  <p> Price: {value.product.price * value.quantity} {value.product.currencyIcon}</p>
+                  {value.title}
+                  <p> Price: {value.price * value.quantity} {value.currencyIcon}</p>
                   <p> Quantity: {value.quantity}</p>
                 </Figure.Caption>
               </Row>
--- fn/client/src/reducers/Cart-reducer.js
+++ fn/client/src/reducers/Cart-reducer.js
@@ -6,11 +6,19 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PRODUCT_CART':
-      console.log(state.products);
+      let newProducts = [...state.products];
+
+      let foundProduct = newProducts.find(value => action.payload.id === value.id);
+      if (foundProduct) {
+        foundProduct.quantity++;
+      } else {
+        newProducts.push({...action.payload, quantity: 1});
+      }
+
       return {
         ...state,
         cartNumbers: state.cartNumbers + 1,
-        products: [...state.products, action.payload],
+        products: newProducts,
       };
     case 'GET_NUMBERS_CART':
       return {
