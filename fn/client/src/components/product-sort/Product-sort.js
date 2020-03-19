ab933f57c0472747a39c97b23c17677f34279208
bandvov <bandvov@gmail.com>
2020-03-19 13:48:33 +0200

    added react-fontawesome

--- fn/client/src/components/product-sort/Product-sort.js
+++ fn/client/src/components/product-sort/Product-sort.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Product-sort.css';
 import Button from 'react-bootstrap/Button';
+import FA from 'react-fontawesome';
 
 const ProductSort = ({ options }) => {
     const [sortValue, setSortValue] = useState(options[0].value);
@@ -13,7 +14,7 @@ const ProductSort = ({ options }) => {
     }
     const buttons = options.map(({ text, variant }) => (
         <Button key={text} variant={variant} onClick={changeHandler}>
-            {text}  <i className={className}></i>
+            {text}  <FA className={className} />
         </Button>
     ));
     return <div className="flex row flex-wrap">{buttons}</div>;
