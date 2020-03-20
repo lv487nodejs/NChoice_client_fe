69420c9bf0bd593b16d427b045c4c0d0732b9dcc
Volodymyr Trach <vovka.trach@gmail.com>
2020-03-20 14:52:36 +0200

    hotfix

--- fn/client/src/components/app-header-nav-right/App-header-nav-right.js
+++ fn/client/src/components/app-header-nav-right/App-header-nav-right.js
@@ -4,38 +4,39 @@ import { Link } from 'react-router-dom';
 import './App-header-nav-right.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faUser, faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faUser, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import Currency from '../currency';
 import { connect } from 'react-redux';
 
-const AppHeaderNavRight = ({cartNumbers}) => {
+const AppHeaderNavRight = ({ cartNumbers }) => {
 
   return (
     <nav className="nav-bar">
-        <ul>
-            <li key="4">
-                <Link to="/wishlist">
-                    <FontAwesomeIcon icon={faHeart} />
-                </Link>
-            </li>
-            <li key="5">
-                <span className="currency-button"><Currency /></span>
-            </li>
-            <li key="6">
-                <Link to="/login">
-                    <FontAwesomeIcon icon={faUser} />
-                </Link>
-    <li key="7">
+      <ul>
+        <li key="4">
+          <Link to="/wishlist">
+            <FontAwesomeIcon icon={faHeart} />
+          </Link>
+        </li>
+        <li key="5">
+          <span className="currency-button"><Currency /></span>
+        </li>
+        <li key="6">
+          <Link to="/login">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+          </li>
+        <li key="7">
           <Link to="/cart">
-            <FontAwesomeIcon icon={faShoppingBasket}/>
+            <FontAwesomeIcon icon={faShoppingBasket} />
             <span> <sup>{cartNumbers}</sup> </span>
           </Link>
-            </li>
-        </ul>
+        </li>
+      </ul>
     </nav>
   )
 };
 
-const mapStateToProps =  ({ cartReducer: {cartNumbers}}) => ({cartNumbers});
+const mapStateToProps = ({ cartReducer: { cartNumbers } }) => ({ cartNumbers });
 
 export default connect(mapStateToProps)(AppHeaderNavRight);
