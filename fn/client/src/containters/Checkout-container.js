103babf46949f8817d399f45c302407381867c13
Volodymyr Trach <vovka.trach@gmail.com>
2020-03-24 23:41:57 +0200

    created route to checkout-container

---
+++ fn/client/src/containters/Checkout-container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Checkout-container.css'
+
+const CheckoutContainer = () => {
+    return (
+        <div>
+          <h1>HELLLOOOOOOO I`m checkout!!!!!!!!!!!!!!!!</h1>  
+        </div>
+    )
+}
+export default CheckoutContainer;
