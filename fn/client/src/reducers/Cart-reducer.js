3419bf7ebcea412958264f7bf3e87a7b8da8deb8
Volodymyr Trach <vovka.trach@gmail.com>
2020-03-28 00:19:10 +0200

    merged with master

--- fn/client/src/reducers/Cart-reducer.js
+++ fn/client/src/reducers/Cart-reducer.js
@@ -5,6 +5,7 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  
   switch (action.type) {
     case 'ADD_PRODUCT_TO_CART':
       let newProducts = [...state.products];
