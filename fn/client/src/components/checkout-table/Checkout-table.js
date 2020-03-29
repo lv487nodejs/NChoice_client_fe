38205aba07fc2f3f9764e242867a11829806cf1d
Volodymyr Trach <vovka.trach@gmail.com>
2020-03-30 02:27:47 +0300

    added change currency to order page

--- fn/client/src/components/checkout-table/Checkout-table.js
+++ fn/client/src/components/checkout-table/Checkout-table.js
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Checkout-table.css';
 import { Table } from 'react-bootstrap'
 import { connect } from 'react-redux';
 
 
-const CheckoutTable = ({ products, currency }) => (
-    <Table
-        bordered
-        hover
-        responsive>
-        <thead>
-            <tr>
-                <th>Image</th>
-                <th>Item name</th>
-                <th>Quantity</th>
-                <th>Item prise</th>
-            </tr>
-        </thead>
-        <tbody>
-            {products.map(productRow =>
-                <tr key={productRow.id}>
-                    <td>
-                        <img
-                            className="order-tbl-img"
-                            src={`/images/products/${productRow.images}`} />
-                    </td>
-                    <td>{productRow.title}</td>
-                    <td>{productRow.quantity}</td>
-                    <td>{Math.round(productRow.price * productRow.quantity * currency).toFixed(2)}</td>
+const CheckoutTable = ({ products, currency }) => {
+
+    const [currencyIcon, setCurrencyIcon] = useState();
+    
+    useEffect(() => {
+        currency === 1 ? setCurrencyIcon('€') : setCurrencyIcon('$');
+    }, [currency]);
+
+    return (
+        <Table
+            bordered
+            hover
+            responsive>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Item name</th>
+                    <th>Quantity</th>
+                    <th>Item prise</th>
                 </tr>
-            )}
-            <tr>
-                <td colSpan="3">Total:</td>
-                <td>1000!!!</td>
-            </tr>
-        </tbody>
-    </Table>
-);
+            </thead>
+            <tbody>
+                {products.map(productRow =>
+                    <tr key={productRow.id}>
+                        <td>
+                            <img
+                                className="order-tbl-img"
+                                src={`/images/products/${productRow.images}`} />
+                        </td>
+                        <td>{productRow.title}</td>
+                        <td>{productRow.quantity}</td>
+                        <td>{`${Math.round(productRow.price * productRow.quantity * currency).toFixed(2)} ${currencyIcon}`}</td>
+                    </tr>
+                )}
+                <tr>
+                    <td colSpan="3">Total:</td>
+                    <td>1000!!!</td>
+                </tr>
+            </tbody>
+        </Table>
+    );
+
+}
+
 
 
 const mapStateToProps = ({ cartReducer: { products }, productsList: { currency } }) => ({
