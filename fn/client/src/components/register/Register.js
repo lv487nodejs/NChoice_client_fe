a8f451cf66bf44933b26b0ad9b4d4e94f2a83f80
oksanalutsyk <oksanalutsyk95@gmail.com>
2020-04-08 00:42:53 +0300

    login and register changes

--- fn/client/src/components/register/Register.js
+++ fn/client/src/components/register/Register.js
@@ -6,20 +6,17 @@ import { connect } from "react-redux";
 import { REGISTER_ROUTE } from "../../configs/login-register-config";
 import axios from "axios";
 import { setUserLogged, setUserLoading } from "../../actions";
-
-
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import LoadingSpinner from "../Loading-spinner";
+import { SignupSchemaRegister } from '../../configs/login-register-config'
 
 
 const addDataToLocalStorage = (token) => {
     localStorage.setItem('accessToken', JSON.stringify(token.accessToken));
     localStorage.setItem('refreshToken', JSON.stringify(token.refreshToken));
-    localStorage.setItem('userId', JSON.stringify(token.id))
+    localStorage.setItem('userId', JSON.stringify(token.user._id))
 }
 
 const USER_DATA = {
@@ -30,61 +27,13 @@ const USER_DATA = {
 }
 
 const eye = <FontAwesomeIcon icon={faEye} />;
-const firstNameRequiredMessage = "No first name provided";
-const firstNameMinElementCount = 2;
-const firstNameMinMessage = `First name is too short - should be ${firstNameMinElementCount} chars minimum`;
-const lastNameRequiredMessage = "No last name provided";
-const lastNameMinElementCount = 2;
-const lastNameMinMessage = `Last name is too short - should be ${lastNameMinElementCount} chars minimum`;
-const emailRegExp = new RegExp(/^([a-z0-9_-]+.)[a-z0-9_-]+@[a-z0-9_-]+(.[a-z0-9_-]+).[a-z]{2,6}$/);
-const emailRegExpMessage = "Email must be correct. Example: nick@mail.com";
-const emailRequiredMessage = "Required";
-const passwordRegExp = new RegExp(/(?=.*[0-9])/);
-const passwordRegExpMessage = "Password must contain a number";
-const passwordRequiredMessage = "No password provided";
-const passwordMinElementCount = 8;
-const passwordMinMessage = `Password is too short - should be ${passwordMinElementCount} chars minimum`;
-const passwordConfirmMessage = 'Please, confirm your password';
-const agreeToTermsMessage = 'You must agree to terms to continue';
-const SignupSchema = yup.object().shape({
-    firstName: yup
-        .string()
-        .required(firstNameRequiredMessage)
-        .min(firstNameMinElementCount, firstNameMinMessage),
-    lastName: yup
-        .string()
-        .required(lastNameRequiredMessage)
-        .min(lastNameMinElementCount, lastNameMinMessage),
-    email: yup
-        .string()
-        .required(emailRequiredMessage)
-        .matches(emailRegExp, emailRegExpMessage),
-    password: yup
-        .string()
-        .required(passwordRequiredMessage)
-        .min(passwordMinElementCount, passwordMinMessage)
-        .matches(passwordRegExp, passwordRegExpMessage),
-    confirmPassword: yup
-        .string()
-        .required(passwordRequiredMessage)
-        .test('passwords-match', passwordConfirmMessage, function (value) {
-            return this.parent.password === value;
-        }),
-    agreeToTerms: yup
-        .boolean()
-        .label('Terms')
-        .test(
-            'is-true',
-            agreeToTermsMessage,
-            value => value === true
-        ),
-});
+
 const Register = (props) => {
     const [user, setUser] = useState(USER_DATA);
     const { setUserLogged, setUserLoading, userLogged, userLoading } = props;
     const [errorMsg, setErrorMsg] = useState('');
     const { register, handleSubmit, errors } = useForm({
-        validationSchema: SignupSchema
+        validationSchema: SignupSchemaRegister
     });
     const [passwordShown, setPasswordShown] = useState(false);
     const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
@@ -117,24 +66,7 @@ const Register = (props) => {
             setErrorMsg(msg)
         }
     }
-    // const postUser = (value, route) => {
-    //     postUserStarted();
-    //     axios({
-    //         method: 'post',
-    //         url: route,
-    //         data: value
-    //     }).then(response => {
-    //         const { accessToken, refreshToken } = response.data;
-    //         return { accessToken, refreshToken };
-    //     }).then(json => {
-    //         postUserSuccess(json);
-    //         addDataToLocalStorage(json);
-    //     }).catch(e => {
-    //         console.log(e);
-    //         postUserError(e);
-    //     });
-    // }
-
+   
     const handleOnSubmit = (event) => {
         postUser(user, REGISTER_ROUTE);
     };
@@ -155,7 +87,6 @@ const Register = (props) => {
                     <Form.Control
                         type="text"
                         placeholder="First name"
-                        defaultValue="Mark"
                         name={'firstName'}
                         value={user.firstName}
                         onChange={handleChange}
@@ -168,7 +99,6 @@ const Register = (props) => {
                     <Form.Control
                         type="text"
                         placeholder="Last name"
-                        defaultValue="Otto"
                         name={'lastName'}
                         value={user.lastName}
                         onChange={handleChange}
@@ -179,7 +109,7 @@ const Register = (props) => {
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
-                            type="email"
+                            type="text"
                             placeholder="Enter email"
                             name={'email'}
                             value={user.email}
@@ -200,6 +130,7 @@ const Register = (props) => {
                                 value={user.password}
                                 onChange={handleChange}
                                 ref={register}
+                                autoComplete="on"
                             />
                             <i onClick={togglePasswordVisiblity}>{eye}</i>
                         </Form.Group>
@@ -208,10 +139,11 @@ const Register = (props) => {
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label> Confirm Password</Form.Label>
                         <Form.Group className="pass-wrapper">
-                        <Form.Control type="password" placeholder="Password"
+                        <Form.Control placeholder="Password"
                             name={'confirmPassword'}
                             ref={register}
                             type={confirmPasswordShown ? "text" : "password"}
+                            autoComplete="on"
                         />
                         <i onClick={toggleConfirmPasswordVisiblity}>{eye}</i>
                         </Form.Group>
@@ -232,7 +164,7 @@ const Register = (props) => {
                         <Button variant="dark" type="submit" block>
                             REGISTER
                         </Button>
-                        <span>{errorMsg}</span>
+                        <span className="errorMessage">{errorMsg}</span>
                     </Form.Group>
                     <Form.Group className="link">
                         <Link to="/login" className="btn btn-link" >LOG IN</Link>
