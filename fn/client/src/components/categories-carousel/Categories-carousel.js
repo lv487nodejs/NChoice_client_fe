12b885fc3c03ee36be7b7128fddc6b77a500955a
Markiyan Shestakovych <markiyan89@gmail.com>
2020-04-11 21:16:41 +0300

    carousel fix

--- fn/client/src/components/categories-carousel/Categories-carousel.js
+++ fn/client/src/components/categories-carousel/Categories-carousel.js
@@ -1,72 +1,49 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Carousel } from "react-bootstrap";
+import { connect } from "react-redux";
+
 import "./Categories-carousel.css";
 import withStoreService from "../hoc";
-import connect from "react-redux/es/connect/connect";
-import { filterAddBrand, filterRemoveAllBrands, setCatalogFilter} from "../../actions";
+
+import { filterAddBrand, filterRemoveAllBrands} from "../../actions";
 import { PRODUCT_LIST_URL } from "../../configs/frontend-config";
-import { Link } from "react-router-dom";
 
-const CategoriesCarousel = ({ storeService, catalog, filterAddBrand, filterRemoveAllBrands, setCatalogFilter}) => {
-  const [getBrands, setBrands] = useState([]);
-  const [getProd, setProd] = useState([]);
+const captionStyle = { textTransform: 'capitalize' }
 
-  let brands = getBrands.map((item) => item.brand);
+const CategoriesCarousel = ({ storeService, catalog, filterAddBrand, filterRemoveAllBrands}) => {
+  const [brands, setBrands] = useState([]);
+
   useEffect(() => {
     storeService
       .getAllBrands()
       .then((response) => setBrands(response))
-      .catch((err) => console.log(err));
   }, [storeService]);
 
-  let productsArray = [];
-  useEffect(() => {
-    setCatalogFilter(catalog)
-    brands.forEach((item) => {
-      storeService.getProductsByFilter({ catalog, brand: item, postsPerPage: 1 })
-        .then((res) => {
-          setProd([...productsArray, res.products[0]]);
-          productsArray.push(res.products[0]);
-        }).catch((e) => console.log(e));
-      setProd([...getProd, ...productsArray]);
-    });
-  }, [
-    storeService,
-    getBrands,
-    catalog
-  ]);
-  // useEffect(() => () => {filterRemoveAllBrands('')}, [filterRemoveAllBrands, filterAddBrand ]);
-
   const filterAddBrandHandler = (item) => () => {
-    if (filterRemoveAllBrands()){
-      filterAddBrand(item);
-    }
+    filterRemoveAllBrands();
+    filterAddBrand(item);
   };
 
   return (
     <div>
-      <Carousel controls={true}>
-        {getProd.map((item) => (
-          <Carousel.Item key={item.id} className="carousel">
+      <Carousel>
+        {brands.map((item) => (
+          <Carousel.Item key={item.brand} className="carousel">
             <Link
               to={PRODUCT_LIST_URL + catalog}
               onClick={filterAddBrandHandler(item.brand)}
             >
-              <img className="d-block w-100 img-carousel" alt={item.images[0]} src={`/images/products/${item.images[0]}`}/>
+              <img className="d-block w-100 img-carousel" alt={item.images[0][catalog]} src={`/images/brands/${item.images[0][catalog]}`}/>
             </Link>
-            <Carousel.Caption> <h2>{item.brand}</h2> </Carousel.Caption>
+            <Carousel.Caption> <h2 style={captionStyle}>{item.brand}</h2> </Carousel.Caption>
           </Carousel.Item>
         ))}
       </Carousel>
     </div>
   );
 };
-const mapStateToProps = ({ filter: { getBrands } }) => ({ getBrands });
 
-const mapDispatchToProps = (dispatch) => ({
-  filterAddBrand: (brand) => dispatch(filterAddBrand(brand)),
-  filterRemoveAllBrands: () =>dispatch(filterRemoveAllBrands()),
-  setCatalogFilter: (catalog) => dispatch(setCatalogFilter(catalog))
-});
+const mapDispatchToProps = {filterAddBrand, filterRemoveAllBrands};
 
-export default withStoreService()(connect(mapStateToProps, mapDispatchToProps)(CategoriesCarousel));
+export default withStoreService()(connect(null, mapDispatchToProps)(CategoriesCarousel));
