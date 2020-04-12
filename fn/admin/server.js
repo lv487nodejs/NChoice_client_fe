99f5256aa1635ff7eaf0b983de0fa59205f06b9e
mandzyuk-maryan <mandzyuk.maryan@gmail.com>
2020-04-12 23:06:31 +0300

    add serve file for admin panel

--- fn/admin/server.js
+++ fn/admin/server.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
 app.get('*', (req, res) => {
-    res.redirect('/');
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
