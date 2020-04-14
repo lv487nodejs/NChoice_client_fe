0f36adb475c47711c70ece89bd251017a0df351f
oksanalutsyk <oksanalutsyk95@gmail.com>
2020-04-14 11:58:59 +0300

    create bootstrap navBar

--- fn/client/src/components/app-header/App-header.js
+++ fn/client/src/components/app-header/App-header.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './App-header.css';
 
 import { Link } from 'react-router-dom';
+import { Navbar, Nav} from 'react-bootstrap';
 
 import AppHeaderNavLeft from '../app-header-nav-left';
 import AppHeaderNavRight from '../app-header-nav-right';
 
 const AppHeader = () => (
-    <header className="header">
-        <AppHeaderNavLeft />
-        <Link to="/">
-            <img className="logo" src="/images/logo.svg" alt="Logo" />
-        </Link>
-        <AppHeaderNavRight />
+    <header >
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <AppHeaderNavLeft />
+                </Nav>
+                <Link to="/"><Navbar.Brand className="logo"></Navbar.Brand></Link>
+
+                <Nav>
+                    <AppHeaderNavRight />
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     </header>
 );
 
