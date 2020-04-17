39f80f3542bed008c9496e8fefed975766270576
chak-kit <kitchakk93@gmail.com>
2020-04-17 17:14:10 +0300

    add test to carousel component

---
+++ fn/client/cypress/integration/examples/carousel.spec.js
@@ -0,0 +1,63 @@
+describe("carousel", () => {
+  it("Check carousel images", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+
+    cy.get(".categories-nav").should("be.visible");
+    cy.get(".active > .carousel-caption > h2").should("be.visible");
+    cy.get(".active > a > .d-block").should("be.visible");
+
+    cy.get(".img-carousel").each($el => {
+      cy.wrap($el).should("exist");
+    });
+    cy.get(".img-carousel").each($el => {
+      cy.wrap($el).should("have.attr", "alt");
+    });
+    cy.get(".carousel-caption").each($el => {
+      cy.wrap($el).should("exist");
+    });
+
+    cy.get('.carousel-indicators').children().should('have.length', 10);
+    cy.get('.categories-nav').children().should('have.length', 7);
+
+    cy.get('.category-item').each($el => {
+      cy.wrap($el).children(7).should('have.length', 1);
+    });
+
+
+  });
+
+  it("should display all categories only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(1) > a").click();
+  });
+  it("should display hoodies only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(2) > a").click();
+  });
+  it("should display jeans only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(3) > a").click();
+  });
+  it("should display t-shirts only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(4) > a").click();
+  });
+  it("should display shoes only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(5) > a").click();
+  });
+  it("should display shirts only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(6) > a").click();
+  });
+  it("should display sweaters only", () => {
+    cy.visit("/catalogs/men").viewport(1280, 720);
+    cy.get(".categories-nav > :nth-child(7) > a").click();
+  });
+
+});
+
+
+
+
+
