be13bb871705118fec94e7b7367c027263fba433
chak-kit <kitchakk93@gmail.com>
2020-04-20 20:20:08 +0300

    add cart-lacalstorage.spec

--- fn/client/cypress/integration/examples/cart-localstorage.spec.js
+++ fn/client/cypress/integration/examples/cart-localstorage.spec.js
@@ -2,7 +2,7 @@ describe("Check cart", () => {
   it("should be empty when your open cart at the first time", () => {
     cy.visit("/cart");
     expect([]).to.be.empty;
-    expect(localStorage.getItem("products-collection")).not.exist;
+    expect(localStorage.getItem("products-collection")).to.be.null;
   });
 });
 
@@ -14,7 +14,7 @@ describe("Check cart localStorage", () => {
 
     cy.get(".size > :nth-child(1) > span").click();
     cy.get(".button.btn").dblclick().should(() => {
-      expect(localStorage.getItem("products-collection")).to.exist;
+      expect(localStorage.getItem("products-collection")).not.be.null;
       expect(localStorage.getItem("cart-numbers")).to.eq("2");
     });
   });
@@ -24,7 +24,7 @@ describe("Check wishlist localStorage", () => {
   it("should be displayed added products in the wishlist localStorage", () => {
     cy.visit("products/5e8da23ed9a7af033c9b1cca");
     cy.get('.buttons > .svg-inline--fa > path').click().should(() =>
-      expect(localStorage.getItem("wishlist-collection")).to.exist
+      expect(localStorage.getItem("wishlist-collection")).not.be.null
     )
   });
 });
