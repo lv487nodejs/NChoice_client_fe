ba34bcf1b7589431a2fccee552f800af369c7e8c
Volodymyr Trach <vovka.trach@gmail.com>
2020-04-20 18:36:31 +0300

    refactor paginator

--- fn/client/src/components/product-list-paginator/Product-list-paginator.js
+++ fn/client/src/components/product-list-paginator/Product-list-paginator.js
@@ -7,24 +7,25 @@ function ProductListPaginator({ pagesCount, paginate, currentPage }) {
     for (let i = 1; i <= pagesCount; i += 1) {
         pageNumbers.push(i);
     }
+    const pageNumbersView = pageNumbers.map(number => {                    
+        let activeClass = ''
+        number - 1 === currentPage ? activeClass = 'active' : activeClass = '';
+        return (
+            <li
+                key={number}
+                className={`list-group-item-dark page-item page-link paginator-buttons
+                ${activeClass}`}
+                onClick={() => {
+                    paginate(number)
+                }}>
+                {number}
+            </li>
+        )
+    });
     return (
         <nav>
             <ul className="pagination">
-                {pageNumbers.map(number => {                    
-                    let activeClass = ''
-                    number - 1 === currentPage ? activeClass = 'active' : activeClass = '';
-                    return (
-                        <li
-                            key={number}
-                            className={`list-group-item-dark page-item page-link paginator-buttons ${activeClass}`}
-                            onClick={() => {
-                                paginate(number)
-                            }}
-                        >
-                            {number}
-                        </li>
-                    )
-                })}
+                {pageNumbersView}
             </ul>
         </nav>
     );
