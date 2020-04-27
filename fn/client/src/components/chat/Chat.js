a93787b8d4de29ef0fc16107d92929c9214054f3
bandvov <bandvov@gmail.com>
2020-04-27 17:59:06 +0300

    added chat

---
+++ fn/client/src/components/chat/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CustomerChat from 'react-customer-chat';
+
+const Chat = () => {
+    return (
+        <CustomerChat pageId="100318745007370" />
+    )
+}
+
+export default Chat;
