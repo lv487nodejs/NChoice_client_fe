b4dfffb27096b40c5d4cdbb51a5659738bbe28aa
Volodymyr Trach <vovka.trach@gmail.com>
2020-04-29 02:34:52 +0300

    added useeffect

--- fn/client/src/components/news-list-item/News-list-item.js
+++ fn/client/src/components/news-list-item/News-list-item.js
@@ -6,6 +6,7 @@ const NewsListItem = () => {
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const date = new Date().toLocaleDateString("en-US", options)
     return (
+        <>
         <section className="news-card">
             <Card.Img className="card-img" variant="top" src="/images/oops.jpg" />
             <Card.Body>
@@ -21,6 +22,37 @@ const NewsListItem = () => {
                 </div>
                 <small className="text-muted">{date}</small>
         </section>
+        <section className="news-card">
+            <Card.Img className="card-img" variant="top" src="/images/oops.jpg" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </Card.Text>
+            </Card.Body>
+                <div>
+                    <small id="loading" className="author">by Volodymyr Trach</small>
+                    <Image src="images/dollar.png" roundedCircle />
+                </div>
+                <small className="text-muted">{date}</small>
+        </section>
+        <section className="news-card">
+            <Card.Img className="card-img" variant="top" src="/images/oops.jpg" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </Card.Text>
+            </Card.Body>
+                <div>
+                    <small id="loading" className="author">by Volodymyr Trach</small>
+                    <Image src="images/dollar.png" roundedCircle />
+                </div>
+                <small className="text-muted">{date}</small>
+        </section>
+        </>
     )
 }
 
--- fn/client/src/components/news-list/News-list.js
+++ fn/client/src/components/news-list/News-list.js
@@ -1,11 +1,23 @@
 import React, { useEffect } from 'react'
 import './News-list.css';
+import { connect } from 'react-redux';
 import NewsListItem from '../news-list-item/News-list-item';
+import withStoreService from '../hoc';
+import {
+    productsLoadingStart,
+    productsLoadingStop
+} from '../../actions';
 
-const NewsList = () => {
-    useEffect(()=>{
-        
-    })
+
+const NewsList = ({ storeService, loading }) => {
+    useEffect(() => {
+        productsLoadingStart()
+        storeService.getAllNews().then(news => {
+            console.log(news);   
+        })
+    }, [storeService])
+
+
     return (
         <div className="news-cardDeck">
             <NewsListItem />
@@ -13,4 +25,16 @@ const NewsList = () => {
     )
 }
 
-export default NewsList
+const mapStateToProps = ({
+    productsList: { loading },
+}) => ({
+    loading
+});
+const mapDispatchToProps = {
+    productsLoadingStart,
+    productsLoadingStop
+};
+
+export default withStoreService()(
+    connect(mapStateToProps, mapDispatchToProps)(NewsList)
+);
