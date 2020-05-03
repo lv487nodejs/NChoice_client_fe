e52dd24bbb0acccdf4e49d0b7aac28cfb7bf2ffd
Volodymyr Trach <vovka.trach@gmail.com>
2020-05-03 19:15:20 +0300

    fixed checkout and pdp scroll

--- fn/client/src/components/checkout-form/checkout-form.js
+++ fn/client/src/components/checkout-form/checkout-form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { Jumbotron, Form, Button, Col, Row, Container } from 'react-bootstrap'
 import { connect } from 'react-redux'
@@ -24,6 +24,11 @@ const CheckoutForm = ({
     setSnackbarText,
     setOrderToStore,
 }) => {
+
+    useEffect(() => {
+        window.scrollTo(0,0)
+      }, []);
+    
 
     const [validated, setValidated] = useState(false);
     const [order, setOrder] = useState(orderStore);
@@ -133,7 +138,7 @@ const CheckoutForm = ({
 
     return (
         <Container>
-            <Row>
+            <Row id="checkout-row">
                 <Col>
                     <Jumbotron className="jumbo">
                         <h2>Order Form</h2>
--- fn/client/src/components/product-details/Product-details.js
+++ fn/client/src/components/product-details/Product-details.js
@@ -62,6 +62,7 @@ const ProductDetails = ({
 
   useEffect(() => {
     productsLoadingStart()
+    window.scrollTo(0,0)
     if (!products.length) {
       storeService.getAllProducts().then((res) => setProducts(res))
     }
@@ -94,7 +95,6 @@ const ProductDetails = ({
   if (loading) {
     return <LoadingSpinner />
   }
-
   return (
     <Card className="wrapperDetails" id="wrapper">
       <Card.Body className="cardBody">
