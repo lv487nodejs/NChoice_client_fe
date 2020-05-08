a443911e5564853defd65c5c2e71dd1e084427e0
Volodymyr Trach <vovka.trach@gmail.com>
2020-05-08 14:30:47 +0300

    localhost master

--- fn/client/src/configs/frontend-config.js
+++ fn/client/src/configs/frontend-config.js
@@ -45,7 +45,7 @@ export const deliveryType = {
         "delivery servise"
     ]
 };
-export const _apiBase = 'https://lv487node-backend.herokuapp.com/';
+export const _apiBase = 'http://localhost:5000/';
 
 export const socialNetworksURL = {
     telegram: 'https://web.telegram.org',
