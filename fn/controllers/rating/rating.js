7889fe0ea45ae3f220e9b6c6cf233e8a8a069871
Volodymyr Trach <vovka.trach@gmail.com>
2020-05-14 19:13:03 +0300

    fix filter

--- fn/controllers/rating/rating.js
+++ fn/controllers/rating/rating.js
@@ -2,14 +2,14 @@ const asyncHandler = require('../../middleware/async');
 const Product = require('../../models/Product');
 const ErrorResponse = require('../../utils/errorResponse');
 
-const updateRating = asyncHandler(async (req, res,next) => {
+const updateRating = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
-    const { rate } = req.body;    
+    const { rate } = req.body;
     const productToUpdate = await Product.findById(id);
-if (!productToUpdate) {
+    if (!productToUpdate) {
         return next(new ErrorResponse('Product not found', 404));
     }
-    productToUpdate.rate.push(+rate);
+    productToUpdate.rate = rate;
     productToUpdate.save();
     res.status(200).send('rating updated');
 })
