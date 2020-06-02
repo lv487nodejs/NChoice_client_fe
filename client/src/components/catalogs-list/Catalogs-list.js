86d4a1744dcd740a6d08f9a2e857238d3d027281
Dmytro Didukh <jeriko.outback@gmail.com>
2020-06-02 17:28:18 +0300

    made html-css logo

--- client/src/components/catalogs-list/Catalogs-list.js
+++ client/src/components/catalogs-list/Catalogs-list.js
@@ -1,41 +1,50 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import './Catalogs-list.css';
 
-import { connect } from 'react-redux';
-import { catalogsLoaded, catalogsRequested } from '../../actions';
+import {connect} from 'react-redux';
+import {catalogsLoaded, catalogsRequested} from '../../actions';
 import withStoreService from '../hoc';
 import CatalogsListItem from '../catalogs-list-item/Catalogs-list-item';
 import LoadingSpinner from '../Loading-spinner';
 
 const CatalogsList = ({
-  storeService,
-  catalogsLoaded,
-  catalogsRequested,
-  catalogs,
-  loading
-}) => {
-  useEffect(() => {
-    catalogsRequested();
-    storeService.getAllCatalogs().then((res) => catalogsLoaded(res));
-  }, [catalogsLoaded, catalogsRequested, storeService]);
+                          storeService,
+                          catalogsLoaded,
+                          catalogsRequested,
+                          catalogs,
+                          loading
+                      }) => {
+    useEffect(() => {
+        catalogsRequested();
+        storeService.getAllCatalogs().then((res) => catalogsLoaded(res));
+    }, [catalogsLoaded, catalogsRequested, storeService]);
 
-  if (loading) {
-    return <LoadingSpinner />;
-  }
+    if (loading) {
+        return <LoadingSpinner/>;
+    }
 
-  const items = catalogs.map((catalog) => (
-    <CatalogsListItem key={catalog._id} catalog={catalog.catalog} />
-  ));
+    const items = catalogs.map((catalog) => (
+        <CatalogsListItem key={catalog._id} catalog={catalog.catalog}/>
+    ));
 
-  return <div> <div className='home-page-bg'/><div className="catalogs">{items}</div> </div>;
+    return <div>
+        <div className='home-page-bg'>
+            <div className='home-page-bg__logo'>
+                <h6 className='home-page-bg__logo__row-1'>designed</h6>
+                <h1 className='home-page-bg__logo__row-2'>horondi</h1>
+                <h5 className='home-page-bg__logo__row-3'>made in lviv</h5>
+            </div>
+        </div>
+        <div className="catalogs">{items}</div>
+    </div>;
 };
 
-const mapStateToProps = ({ catalogsList: { catalogs, loading } }) => ({
-  catalogs,
-  loading
+const mapStateToProps = ({catalogsList: {catalogs, loading}}) => ({
+    catalogs,
+    loading
 });
-const mapDispatchToProps = { catalogsLoaded, catalogsRequested };
+const mapDispatchToProps = {catalogsLoaded, catalogsRequested};
 
 export default withStoreService()(
-  connect(mapStateToProps, mapDispatchToProps)(CatalogsList)
+    connect(mapStateToProps, mapDispatchToProps)(CatalogsList)
 );
