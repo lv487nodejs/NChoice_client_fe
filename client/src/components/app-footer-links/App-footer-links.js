009468614d900d936d7491e96fdffd811394bc5b
YarkoSumyk <yarko.sumyk@gmail.com>
2020-06-04 12:07:06 +0300

    'fixedLinks'

--- client/src/components/app-footer-links/App-footer-links.js
+++ client/src/components/app-footer-links/App-footer-links.js
@@ -16,13 +16,13 @@ const AppFooterLinks = () => (
         <div className='footer-icons-box'>
             <div className='footer-card-title'>{socialNetworkLinks.title}</div>
             <div>
-                <a className='footer-icon' href={socialNetworkLinks.telegram} target='_blank'>
+                <a className='footer-icon' href={socialNetworkLinks.telegram} target='_blank' rel='noopener noreferrer'>
                     <FontAwesomeIcon className="icon" icon={faTelegramPlane} />
                 </a>
-                <a className='footer-icon' href={socialNetworkLinks.instagram} target='_blank'>
+                <a className='footer-icon' href={socialNetworkLinks.instagram} target='_blank' rel='noopener noreferrer'>
                     <FontAwesomeIcon className="icon" icon={faInstagram} />
                 </a>
-                <a className='footer-icon' href={socialNetworkLinks.facebook} target='_blank'>
+                <a className='footer-icon' href={socialNetworkLinks.facebook} target='_blank' rel='noopener noreferrer'>
                     <FontAwesomeIcon className="icon" icon={faFacebook} />
                 </a>
             </div>
